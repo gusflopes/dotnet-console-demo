7102b536dc2df1b7d74a2db5e9437d5f5905494c
Gustavo Lopes <gusflopes86@gmail.com>
2023-08-17 05:13:01 -0400

    Adicionando um subconsole em um handler

--- GoalSeekPOC/Handlers.cs
+++ GoalSeekPOC/Handlers.cs
@@ -17,6 +17,31 @@ public class AlternativeHandler : IHandler
 {
     public void Invoke(int param)
     {
-        Console.WriteLine($"Executando Handler Secundário para Opção {param}");
+        Console.Clear();
+        Console.WriteLine($"\nExecutando Handler Secundário para Opção {param}");
+        Console.WriteLine($"Digite um comando em texto...");
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (input == "exit()")
+            {
+                Console.Clear();
+                return; // Sai do Programa
+            }
+
+            if (input == "clear()")
+            {
+                Console.Clear();
+            }
+
+            if (input != null)
+            {
+                // Movido para a função InvokeHandler na classe Opcao
+                // Console.WriteLine($"\n{opcaoSelecionada.Response}");
+                Console.WriteLine($"Foi digitado o seguinte: {input}");
+            }
+            Console.WriteLine("\nDigite o comando pretendido ou digite exit() para sair...");
+            // Console.ReadKey();
+        }
     }
 }
